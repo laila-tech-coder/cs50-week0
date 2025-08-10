1b3eed91562dc61b2b55733f04aad0f0cd2d4cb9
laila-tech-coder <lh0794066@gmail.com>
2025-08-10 17:17:12 +0300

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,50 @@
+#x=1
+#y=2
+
+#z=x+y
+#k=x*y
+
+#print(z)
+#print(k)
+
+#b=int(input("what's b\n"))
+#a=int(input("what's a\n"))
+
+#print(b+a)
+
+
+"""h=float (input("what's h ? \n"))
+n=float (input("what's n ? \n"))
+
+#z=round(h+n)#بنكتب الارقام عادي عشري او صحيح وبتحسبها 
+#print(z)
+
+#y=round(h*n)
+
+
+#print(y)
+
+#m=round(h /n, 2)#يعني اظهرلي رقمين فقط
+
+#print(m)
+
+u=h/n
+
+print(f"{u:.2f}")#f string السلاسل النصيه 
+
+"""
+
+
+def main ( ):
+	x=int(input("what x ?"))
+	print("x squared is " , square (x))
+
+def square( n):
+	return pow (n, 2)#ودي برده تربيع 
+#	return n ** 2
+	
+
+
+main()
+
+
