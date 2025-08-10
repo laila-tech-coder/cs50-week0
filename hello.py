94f770e14d612f965068d58df0b66cedc7f3b9bd
laila-tech-coder <lh0794066@gmail.com>
2025-08-10 17:14:37 +0300

    Create hello.py
    This Python program asks the user for their name and then greets them using a custom function called `hello`. It demonstrates how to define functions, pass arguments, and call them from the main program.

---
+++ hello.py
@@ -0,0 +1,14 @@
+def main( ):
+	name=input("what your name")
+	hello( name)
+
+def hello(to="world"):
+    print("hello", to)
+
+main ( ) #لازم استدعي اورجمنت
+"""name = input("what's your name ? ")
+hello(name)
+
+print(name)
+"""
+
